e977b160afd2adbb3acb625725c0ddc2708349ad
Marcin Kowalski <marcinmac@MacBook-Pro-Marcin.local>
2024-04-20 09:43:54 +0200

    creating AnimatedClass and water animation

--- code/main.py
+++ code/main.py
@@ -2,10 +2,12 @@ from settings import *
 from pytmx.util_pygame import load_pygame
 from os.path import join
 
-from sprites import Sprite
+from sprites import *
+
 from entities import Player
 from groups import AllSprites
 
+from support import *
 class Game:
     def __init__(self):
         pygame.init()
@@ -17,8 +19,8 @@ class Game:
 
 
         self.import_assets()
-       # self.setup(self.tmx_maps['world'], 'house')
-        self.setup(self.tmx_maps['hospital'], 'world')
+        self.setup(self.tmx_maps['world'], 'house')
+       # self.setup(self.tmx_maps['hospital'], 'world')
 
         
                     
@@ -41,23 +43,33 @@ class Game:
     def import_assets(self):
         self.tmx_maps = {'world': load_pygame(join('..', 'data', 'maps', 'world.tmx')),
                          'hospital': load_pygame(join('..', 'data', 'maps', 'hospital.tmx')), }
+        
+        self.overworld_frames = {
+            'water': import_folder('..','graphics', 'tilesets', 'water')
+        }
 
     def setup(self, tmx_map, player_start_pos):
-
+            #world
         for layer in ['Terrain', "Terrain Top"]:
             for x,y, surf in tmx_map.get_layer_by_name(layer).tiles():
                 Sprite((x * TILE_SIZE,y * TILE_SIZE), surf, self.all_sprites)
 
 
-
+    #object
         for obj in tmx_map.get_layer_by_name('Objects'):
            Sprite((obj.x, obj.y), obj.image ,self.all_sprites)
            
  
-
+    #entities
         for obj in tmx_map.get_layer_by_name('Entities'):
             if obj.name == 'Player' and obj.properties['pos'] == player_start_pos:
                self.player = Player((obj.x, obj.y), self.all_sprites)
+    #water
+        for obj in tmx_map.get_layer_by_name('Water'):
+            for x in range(int(obj.x), int(obj.x + obj.width), TILE_SIZE):
+                for y in range(int(obj.y), int(obj.y + obj.height), TILE_SIZE):
+                    AnimatedSprite((x,y), self.overworld_frames['water'], self.all_sprites)
+
 if __name__ == "__main__":
     game = Game()
     game.run()
